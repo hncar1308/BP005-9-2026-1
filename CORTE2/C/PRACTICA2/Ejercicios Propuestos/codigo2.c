14dcaf67bef3628b7a9136f31cc722edb4ccc523
hncar1308 <hncar1308@gmail.com>
2026-04-12 10:09:33 -0500

    Create codigo2.c

---
+++ CORTE2/C/PRACTICA2/Ejercicios Propuestos/codigo2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    for (int fila = 1; fila <= 3; fila++) {
+        for (int columna = 1; columna <= 10; columna++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
