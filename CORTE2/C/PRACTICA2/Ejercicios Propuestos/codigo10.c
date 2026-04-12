7d6300c1bf97d77fd767caea394adf02b31e67b4
hncar1308 <hncar1308@gmail.com>
2026-04-12 10:27:24 -0500

    Update codigo10.c

--- CORTE2/C/PRACTICA2/Ejercicios Propuestos/codigo10.c
+++ CORTE2/C/PRACTICA2/Ejercicios Propuestos/codigo10.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    // Techo
+    
     for (int i = 1; i <= 4; i++) {
         for (int j = 1; j <= 7 - i; j++) printf(" ");
         for (int j = 1; j <= (2 * i - 1); j++) printf("^");
         printf("\n");
     }
-    // Base
+    
     for (int i = 1; i <= 4; i++) {
         printf("   | [ ] | \n");
     }
