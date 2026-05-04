11fbdc7877dc239f55fa4d1534c8fedb22407a07
hncar1308 <hncar1308@gmail.com>
2026-05-04 18:18:24 -0500

    Update ejercicio1.cpp

--- CORTE2/C++/FUNCIONES/ejercicio1.cpp
+++ CORTE2/C++/FUNCIONES/ejercicio1.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 float convertirAFahrenheit(float celsius);
 
 int main() {
-    float celsius;
-    cout << "Ingrese la temperatura en Celsius: ";
+    float celsius, fahrenheit;
+
+    cout << "Digite la temperatura en Celsius: ";
     cin >> celsius;
-    cout << "La temperatura en Fahrenheit es: " << convertirAFahrenheit(celsius) << endl;
+
+    fahrenheit = convertirAFahrenheit(celsius);
+
+    cout << fixed << setprecision(2);
+    cout << "Temperatura en Fahrenheit: " << fahrenheit << endl;
+
     return 0;
 }
 
 float convertirAFahrenheit(float celsius) {
-    return (celsius * 9 / 5) + 32;
+    return (9.0 / 5.0 * celsius) + 32;
 }
