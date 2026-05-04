4b8fbb17594b0e4e3ec6d005eed45ccbb228b675
hncar1308 <hncar1308@gmail.com>
2026-05-04 18:08:37 -0500

    Create ejercicio1.c

---
+++ CORTE2/C/FUNCIONES/ejercicio1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float convertirAFahrenheit(float celsius);
+
+int main() {
+    float celsius;
+    printf("Ingrese la temperatura en Celsius: ");
+    scanf("%f", &celsius);
+    printf("La temperatura en Fahrenheit es: %.2f\n", convertirCelsiusAFahrenheit(celsius));
+    return 0;
+}
+
+float convertirAFahrenheit(float celsius) {
+    return (celsius * 9 / 5) + 32;
+}
