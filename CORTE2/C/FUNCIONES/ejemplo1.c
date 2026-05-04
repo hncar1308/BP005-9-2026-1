f079d799f1df547641809380ccbf31ad871075ee
hncar1308 <hncar1308@gmail.com>
2026-05-04 16:50:10 -0500

    Create ejemplo1.c

---
+++ CORTE2/C/FUNCIONES/ejemplo1.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+void saludar(void); 
+  
+int main() 
+{ 
+    saludar(); 
+
+    return 0;
+} 
+
+void saludar(void) 
+{
+    printf("Hola. Estamos estudiando funciones.\n"); 
+}
