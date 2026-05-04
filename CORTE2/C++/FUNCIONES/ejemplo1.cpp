0e6158a4dffa198e75e34b1b897b6a1254b9c4f2
hncar1308 <hncar1308@gmail.com>
2026-05-04 16:52:30 -0500

    Create ejemplo1.cpp

---
+++ CORTE2/C++/FUNCIONES/ejemplo1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void saludar();
+
+int main()
+{
+    saludar();
+    return 0;
+}
+
+void saludar()
+{
+    cout << "Hola. Estamos estudiando funciones." << endl;
+}
