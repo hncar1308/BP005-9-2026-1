a6e38bc0fe32fd7a1bb692d12d9d3c71d12645ce
hncar1308 <hncar1308@gmail.com>
2026-05-22 16:47:08 -0500

    Create codigo2.cpp

---
+++ CORTE2/C++/PRACTICA 2/EJERCICIOS PROPUESTOS/codigo2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int fila = 1; fila <= 3; fila++) {
+        for (int columna = 6; columna <= 10; columna++) {
+            cout << "# ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
