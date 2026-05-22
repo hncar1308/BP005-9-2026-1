dfca5fdffcd3feae1f7d850eaad12af3e7575683
hncar1308 <hncar1308@gmail.com>
2026-05-22 17:41:54 -0500

    Create triangulocreciente3.cpp

---
+++ CORTE2/C++/PRACTICA 2/FIGURAS/triangulocreciente3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fila, columna;
+
+        for (fila = 1; fila <= 5; fila++) {
+                for (columna = 1; columna <= fila; columna++) {
+                        cout << ("@ ");
+                }
+                cout << ("\n");
+        }
+    return 0;
+}
