1b69b1533eba623498d51dffe6032f40853effbc
hncar1308 <hncar1308@gmail.com>
2026-05-22 17:45:14 -0500

    Create piramide4.cpp

---
+++ CORTE2/C++/PRACTICA 2/FIGURAS/piramide4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fila, espacios, simbolos;
+
+        for (fila = 1; fila <= 5; fila++) {
+                for (espacios = 1; espacios <= 5 - fila; espacios++) {
+                        cout << " ";
+                }
+
+                for (simbolos = 1; simbolos <= 2 * fila - 1; simbolos++) {
+                        cout << "*";
+                }
+
+                cout << "\n";
+        }
+    return 0;
+}
