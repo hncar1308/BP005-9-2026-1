5bde71da530104d56f85bd8f733681e597083c67
hncar1308 <hncar1308@gmail.com>
2026-05-26 16:46:28 -0500

    Create ejemplo_02_recorrido_arreglo.cpp

---
+++ CORTE2/C++/Arreglos, matrices y punteros/EJEMPLOS/ejemplo_02_recorrido_arreglo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int datos[5] = {8, 3, 15, 6, 10};
+
+    for (int i = 0; i < 5; i++) {
+        cout << "datos[" << i << "] = " << datos[i] << endl;
+    }
+
+    return 0;
+}
