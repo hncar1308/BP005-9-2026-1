d941dd4d92525f6a76b1062f3a2985d163b44b1f
hncar1308 <hncar1308@gmail.com>
2026-05-26 17:58:03 -0500

    Create etapa_01_lectura_simple_potenciometro.ino

---
+++ CORTE2/C/PRACTICA ARDUINO/etapa_01_lectura_simple_potenciometro.ino
@@ -0,0 +1,13 @@
+const int PIN_POTENCIOMETRO = 12;
+void setup() {
+
+Serial.begin(9600);}
+void loop() {
+
+int valorADC = analogRead(PIN_POTENCIOMETRO);
+
+Serial.print("ADC = ");
+Serial.println(valorADC);
+
+delay(500);
+}
