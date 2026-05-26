dbb768315a45eaa05f6b83321b1f13acf4816294
hncar1308 <hncar1308@gmail.com>
2026-05-26 16:42:43 -0500

    Create ejemplo_01_arreglo_basico.cpp

---
+++ CORTE2/C++/Arreglos, matrices y punteros/ejemplo_01_arreglo_basico.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int datos[5] = {8, 3, 15, 6, 10};
+
+    cout << "datos[0] = " << datos[0] << endl;
+    cout << "datos[1] = " << datos[1] << endl;
+    cout << "datos[2] = " << datos[2] << endl;
+    cout << "datos[3] = " << datos[3] << endl;
+    cout << "datos[4] = " << datos[4] << endl;
+
+    return 0;
+}
