e020fb4083fa49ae1cd7887cd7339dceb62dc53b
hncar1308 <hncar1308@gmail.com>
2026-05-26 16:50:57 -0500

    Update ejemplo_03_direcciones_arreglo.c

--- CORTE2/C/Arreglos, matrices y punteros/EJEMPLOS/ejemplo_03_direcciones_arreglo.c
+++ CORTE2/C/Arreglos, matrices y punteros/EJEMPLOS/ejemplo_03_direcciones_arreglo.c
@@ -5,7 +5,8 @@ int main() {
     int i;
 
     for (i = 0; i < 5; i++) {
-        printf("datos[%d] = %d, direccion = %p\n", i, datos[i], (void *)&datos[i]);
+        printf("datos[%d] = %d, direccion = %p\n", 
+            i, datos[i], (void *)&datos[i]);
     }
 
     return 0;
