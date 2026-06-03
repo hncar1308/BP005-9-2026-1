2f01df3a57efc0a81c7822f28ede3b0a7081aa99
hncar1308 <hncar1308@gmail.com>
2026-06-03 11:27:45 -0500

    Create uint8_t.cpp

---
+++ FINAL/BINARIOS/EJEMPLOS/uint8_t.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main() {
+    uint8_t valor = 65;
+    
+    cout << valor << endl;
+    cout << static_cast<int>(valor) << endl; 
+
+return 0;
+}
