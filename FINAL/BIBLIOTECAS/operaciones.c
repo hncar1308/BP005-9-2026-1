b4908560fdf78dabd4c4b59c96a3a7c4f9d29ebc
hncar1308 <hncar1308@gmail.com>
2026-06-03 15:46:27 -0500

    Create operaciones.c

---
+++ FINAL/BIBLIOTECAS/operaciones.c
@@ -0,0 +1,21 @@
+#include "operaciones.h"
+
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int restar(int a, int b) {
+    return a - b;
+}
+
+int multiplicar(int a, int b) {
+    return a * b;
+}
+
+float dividir(float a, float b) {
+    if (b == 0.0f) {
+        return 0.0f
+    }
+    
+    return a / b;
+}
