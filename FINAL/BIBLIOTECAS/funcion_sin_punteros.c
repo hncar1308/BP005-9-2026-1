fb3918f94e458fdcba902a4806d58ccada601fd2
hncar1308 <hncar1308@gmail.com>
2026-06-03 16:27:43 -0500

    Create funcion_sin_punteros.c

---
+++ FINAL/BIBLIOTECAS/funcion_sin_punteros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void cambiarValor(int x) {
+    x = 100;
+}
+
+int main() {
+    int numero = 20;
+
+    cambiarValor(numero);
+    printf("numero= %d\n", numero);
+
+    return 0;
+}
