401a584a9bca1543c371c7d8677fdd9587553d1c
hncar1308 <hncar1308@gmail.com>
2026-06-03 13:20:48 -0500

    Create ej11.cpp

---
+++ FINAL/BINARIOS/EJERCICIOS_PROPUESTOS/ej11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main() {
+    uint32_t ms_funcionamiento = 3600000;
+
+    cout << "Milisegundos de operación: " << ms_funcionamiento << endl;
+
+    return 0;
+}
