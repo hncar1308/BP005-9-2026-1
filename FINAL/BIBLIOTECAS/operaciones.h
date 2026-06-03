41ba1e1b3fba20027fe5f4db612ba4cbd54474fa
hncar1308 <hncar1308@gmail.com>
2026-06-03 15:42:59 -0500

    Create operaciones.h

---
+++ FINAL/BIBLIOTECAS/operaciones.h
@@ -0,0 +1,12 @@
+#ifndef OPERACIONES_H
+#define OPERACIONES_H
+
+int sumar(int a, int b);
+
+int restar(int a, int b);
+
+int multiplicar(int a, int b);
+
+float dividir(float a, float b);
+
+#endif
