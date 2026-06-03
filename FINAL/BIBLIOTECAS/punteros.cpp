1436386c8b97b2cfbebfdb5a5ff4a8243299334a
hncar1308 <hncar1308@gmail.com>
2026-06-03 16:57:16 -0500

    Create punteros.cpp

---
+++ FINAL/BIBLIOTECAS/punteros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void cambiarValor(int *x) {
+    *x = 100;
+}
+
+int main() {
+    int numero = 20;
+
+    cambiarValor(&numero);
+
+    std::cout << "numero = "
+              << numero
+              << std::endl;
+
+    return 0;
+}
