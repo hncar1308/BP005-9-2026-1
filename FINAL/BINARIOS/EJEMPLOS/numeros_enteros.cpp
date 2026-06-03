e25fb9328a3b475aaab47c707a307ec3c6a031c8
hncar1308 <hncar1308@gmail.com>
2026-06-03 11:34:26 -0500

    Create numeros_enteros.cpp

---
+++ FINAL/BINARIOS/EJEMPLOS/numeros_enteros.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <limits>
+#include <cstdint>
+using namespace std;
+
+int main() {
+    int edad = 18;
+    int estudiantes = 35;
+    int contador = 0;
+    
+    cout << edad << endl;
+    cout << estudiantes << endl;
+    cout << contador << endl;
+    
+return 0;
+}
