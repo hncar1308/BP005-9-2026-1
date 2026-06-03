16797feb2739dacf50b76d54d832690d95ca17e2
hncar1308 <hncar1308@gmail.com>
2026-06-03 13:20:18 -0500

    Create ej10.cpp

---
+++ FINAL/BINARIOS/EJERCICIOS_PROPUESTOS/ej10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main() {
+    uint16_t pulsos_encoder = 45000;
+
+    cout << "Pulsos del encoder: " << pulsos_encoder << endl;
+
+    return 0;
+}
